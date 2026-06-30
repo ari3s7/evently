ab2484854f65235a69ebc5c000519b86d0f741cf
Abhishek Sharma <abhi761685@gmail.com>
2026-06-30 18:51:32 +0530

    add route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import authRoutes from './modules/auth/auth.routes'
+import eventRoute from "./modules/event/event.route"
+import venueRoutes from "./modules/venue/venue.routes"
+
 
 const app=express();
 app.use(express.json());
 
-app.use("/auth", authRoutes )
+app.use("/auth", authRoutes );
+app.use("/event", eventRoute);
+app.use("/venue", venueRoutes);
 
 export default app;
