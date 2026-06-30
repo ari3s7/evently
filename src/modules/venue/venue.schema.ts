4e0113a7e1575b6c800bd642937f1a810025aca7
Abhishek Sharma <abhi761685@gmail.com>
2026-06-30 16:25:53 +0530

    add venue zod validation

---
+++ src/modules/venue/venue.schema.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+
+export const venueSchema = z.object({
+    name: z.string().trim().min(3, "Venue name must be at least 3 characters").max(100, "Venue name cannot exceed 100 characters"),
+    address: z.string().trim().min(3, "Address must be atleast ").max(255, "Venue address cannot exceed 255 characters"),
+    capacity: z.number().int().positive("Capacity must be greater than 0"),
+});
+
+export const updateVenueSchema = venueSchema.partial();
