19bce7a7fa7774b368585dc45258fb89a6737d0b
Abhishek Sharma <abhi761685@gmail.com>
2026-07-01 13:34:48 +0530

    create event zod schema

---
+++ src/modules/event/event.schema.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const eventSchema = z.object({
+    title: z.string().trim().min(3, "Title must be at least 3 characters").max(100, "Title cannot exceed 100 characters"),
+    description: z.string().min(10, "Description must be at least 10 characters").max(1000, "Description cannot exceed 1000 characters"),
+    price: z.number().positive(),
+    startTime: z.iso.datetime(),
+    endTime: z.iso.datetime(),
+    venueId: z.number(),
+    bannerUrl: z.url().optional(),
+ })
