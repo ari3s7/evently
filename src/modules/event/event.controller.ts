86a663b91527a246343ef9ede39808a5c98530d4
Abhishek Sharma <abhi761685@gmail.com>
2026-07-01 13:35:43 +0530

    add event create endpoint

---
+++ src/modules/event/event.controller.ts
@@ -0,0 +1,49 @@
+import type{Request, Response} from 'express';
+import { eventSchema } from './event.schema';
+import { prisma } from "../../config/prisma";
+import { EventStatus } from '../../generated/prisma/enums';
+
+
+export const createEvent = async (req: Request, res: Response) => {
+    const resultEvent = eventSchema.safeParse(req.body);
+
+    if(!resultEvent.success) {
+        return res.status(400).json({
+            success: false,
+            message: "Field Error"
+        });
+    }
+    const { title, description, price, startTime, endTime, venueId, bannerUrl } = resultEvent.data;
+
+    const venue = await prisma.venue.findUnique({
+     where: {
+    id: venueId,
+     },
+   });
+
+  if (!venue) {
+  return res.status(404).json({
+    success: false,
+    message: "Venue not found",
+     });
+   }
+
+    const event = await prisma.event.create({
+        data: {
+            title,
+            description,
+            price,
+            startTime: new Date(startTime),
+            endTime: new Date(endTime),
+            venueId,
+            organizerId: req.user!.id,
+            bannerUrl,
+            
+        }
+    })
+
+    return res.status(201).json({
+     success: true,
+     data: event,
+   });  
+  }
---
+++ src/modules/event/event.route.ts
@@ -0,0 +1,12 @@
+import Router from 'express';
+import { createEvent } from "../event/event.controller"
+import { authenticate } from '../../middleware/auth.middleware';
+import { authorize } from '../../middleware/authorize.middleware';
+import { Role } from '../../generated/prisma/enums';
+
+
+const router = Router();
+
+router.post("/", authenticate, authorize(Role.ADMIN, Role.ORGANIZER), createEvent);
+
+export default router;
