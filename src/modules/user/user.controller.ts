81c0f5b9b6b8145e6a9bd0016020ebfa75b20f7a
Abhishek Sharma <abhi761685@gmail.com>
2026-07-02 19:46:37 +0530

    create user endpoints

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -2,6 +2,84 @@ import type { Request, Response } from "express";
 import { updateRoleSchema, userIdSchema } from "./user.schema";
 import { prisma } from "../../config/prisma";
 
+export const getMe = async(req: Request, res: Response) => {
+    try {
+    const user = await prisma.user.findUnique({
+        where: {
+            id: req.user!.id,
+        },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            role: true,
+            createdAt: true,
+            updatedAt: true,
+        }
+    })
+    if (!user) {
+    return res.status(404).json({
+        success: false,
+        message: "User not found",
+    });
+}
+    return res.status(200).json({
+        success: true,
+        data: user
+    })
+   } catch(error) {
+    return res.status(500).json({
+        success: false,
+        message: "Internal Server error"
+    });
+   } 
+}
+
+export const getAll = async (req: Request, res: Response) =>{
+    try {
+    const user = await prisma.user.findMany();
+
+    return res.status(200).json({
+        success: true,
+        data: user
+    })
+   } catch(error){
+    return res.status(500).json({
+        success: false,
+        message: "Internal Server error"
+    })
+   }
+}
+
+export const getById = async(req: Request, res: Response) => {
+    try {
+    const idResult = userIdSchema.safeParse(req.params);
+
+    if(!idResult.success){
+        return res.status(400).json({
+            success: false,
+            message: "Field Error"
+        });
+    }
+
+    const { id } = idResult.data;
+
+    const user = await prisma.user.findUnique({
+        where: {
+            id,
+        }
+    })
+    return res.status(200).json({
+        success: true,
+        data: user,
+    })
+   } catch(error) {
+    return res.status(500).json({
+        success: false,
+        message: "Internal Server error"
+    });
+   }
+}
 export const updateRole = async (req: Request , res: Response) => {
 
     try {
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -2,11 +2,14 @@ import Router from "express";
 import { authenticate } from "../../middleware/auth.middleware";
 import { authorize } from "../../middleware/authorize.middleware";
 import { Role } from "../../generated/prisma/enums";
-import { updateRole } from "../user/user.controller";
+import { updateRole, getMe, getAll, getById } from "../user/user.controller";
 
 const router = Router();
 
-router.put("/:id/role", authenticate, authorize(Role.ADMIN), updateRole)
+router.put("/:id/role", authenticate, authorize(Role.ADMIN), updateRole);
+router.get("/me", authenticate, getMe);
+router.get("/", authenticate, authorize(Role.ADMIN), getAll);
+router.get("/:id", authenticate, authorize(Role.ADMIN), getById);
 
 
 export default router;
