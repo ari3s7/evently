8d069f4756d2cff28463eb4d6f09baf5ef0c1fd6
Abhishek Sharma <abhi761685@gmail.com>
2026-07-03 18:53:06 +0530

    event routes file rename

---
+++ src/modules/event/event.routes.ts
@@ -0,0 +1,16 @@
+import Router from 'express';
+import { createEvent, getEvents, getEventById, updateEvent, deleteEvent } from "./event.controller"
+import { authenticate } from '../../middleware/auth.middleware';
+import { authorize } from '../../middleware/authorize.middleware';
+import { Role } from '../../generated/prisma/enums';
+
+
+const router = Router();
+
+router.post("/", authenticate, authorize(Role.ADMIN, Role.ORGANIZER), createEvent);
+router.get("/", getEvents);
+router.get("/:id", getEventById)
+router.post("/:id", authenticate, authorize(Role.ADMIN, Role.ORGANIZER),updateEvent)
+router.delete("/:id", authenticate, authorize(Role.ADMIN, Role.ORGANIZER), deleteEvent)
+
+export default router;
