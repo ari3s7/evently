503bb9fdc5b27f6fba259c1ab6f795185533b3e8
Abhishek Sharma <abhi761685@gmail.com>
2026-07-07 20:03:35 +0530

    create user sort schema

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -13,3 +13,13 @@ import { Role } from '../../generated/prisma/enums';
 export const updateRoleSchema = z.object({
     role: z.enum(Role),
 });
+
+export const userSortSchema = z.object({
+    sortBy: z.enum([
+        "name",
+        "email",
+        "createdAt",
+    ]).default("createdAt"),
+
+    order: z.enum(["asc", "desc"]).default("desc"),
+})
