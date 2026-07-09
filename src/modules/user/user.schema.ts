b7199560d8064d427df1eabf2b6459830cc7c3c1
Abhishek Sharma <abhi761685@gmail.com>
2026-07-09 17:57:13 +0530

    add user schema

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { Role } from '../../generated/prisma/enums';
+import { paginationSchema } from '../../common/pagination.schema';
 
 
 
@@ -22,4 +23,19 @@ export const userSortSchema = z.object({
     ]).default("createdAt"),
 
     order: z.enum(["asc", "desc"]).default("desc"),
-})
+});
+
+export const userSearchSchema = z.object({
+  search: z.string().trim().min(1).optional(),
+});
+
+export const userFilterSchema = z.object({
+    role: z.enum(Role).optional(),
+});
+
+export const userQuerySchema = z.object({
+    ...paginationSchema.shape,
+    ...userSortSchema.shape,
+    ...userSearchSchema.shape,
+    ...userFilterSchema.shape,
+});
